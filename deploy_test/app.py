6e2e452c2340b8b61a8f931f260187f1542a69af
JYAN3795 <jyan3795@uni.sydney.edu.au>
2025-01-03 19:12:52 +0800

    login box

--- deploy_test/app.py
+++ deploy_test/app.py
@@ -1,24 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>IPv6 Test</title>
-    </head>
-    <body>
-        <h1>网站部署测试</h1>
-        <p>网站通过Ngrok部署</p>
-        <p>后续开发人员: cjy, yjx</p>
-    </body>
-    </html>
-    '''
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='::', port=80, debug=True)
