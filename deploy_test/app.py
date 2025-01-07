a4d1ff8909234b6ea8f583248e8abc133202353c
JYAN3795 <jyan3795@uni.sydney.edu.au>
2025-01-07 15:40:02 +0800

    Home page

--- deploy_test/app.py
+++ deploy_test/app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def welcome():
     return render_template("Welcome/Welcome.html")
 
 @app.route('/register')
 def register():
     return render_template("Register/Register.html") 
+
+@app.route('/home')  # 添加新路由
+def home():
+    return render_template("Home/Home.html")
 
 if __name__ == '__main__':
     app.run(host='::', port=80, debug=True)
